6a21425d03e0f8430a85896185226fb5312e567b
Sania Arif <sania.arif@columbia.edu>
2016-04-26 00:21:34 -0400

    To convert a sorted list to binary tree

---
+++ Medium/LitsToTree.java
@@ -0,0 +1,48 @@
+/**
+ * Class definition for singly-linked list:
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Class definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class ListToTree {
+    public TreeNode sortedListToBST(ListNode head) 
+    {
+        if(head == null) return null;
+        if(head.next == null) return new TreeNode(head.val);
+        if(head.next.next == null) //Only 2 elements in the tree
+        {
+            TreeNode root = new TreeNode(head.val);
+            root.right = new TreeNode(head.next.val);
+            return root;
+        }
+        
+        ListNode fast=head, slow=head, prev=head;
+        
+        while(fast!=null && fast.next!=null)
+        {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        
+        //slow will be middle- root of tree
+        TreeNode root = new TreeNode (slow.val);
+        
+        prev.next = null;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        
+        return root;
+    }
+}
