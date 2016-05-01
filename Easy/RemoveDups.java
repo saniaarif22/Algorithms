529f114dbead43f534dbfa6a0ad2c6393c63255f
Sania Arif <sania.arif@columbia.edu>
2016-05-01 11:53:52 -0400

    Remove dups from sorted list

---
+++ Easy/RemoveDups.java
@@ -0,0 +1,16 @@
+public class RemoveDups {
+    public ListNode deleteDuplicates(ListNode head) 
+    {
+        ListNode a = head;
+        while (a != null)
+        {
+            if(a.next == null) break;
+            
+            if(a.val == a.next.val) a.next = a.next.next;
+            
+            else a = a.next;
+        }
+        return head;
+        
+    }
+}
