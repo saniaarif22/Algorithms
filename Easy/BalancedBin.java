27053732a43a49b8463f8a999cb535c66b331bee
Sania Arif <sania.arif@columbia.edu>
2016-05-02 01:51:45 -0400

    Definition for a binary tree node.

--- Easy/BalancedBin.java
+++ Easy/BalancedBin.java
@@ -1,3 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class BalancedBin {
 
     //METHOD 1:
