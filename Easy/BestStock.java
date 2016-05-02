c025780d95ec092d1b0c6ecaacd5a416fb867367
Sania Arif <sania.arif@columbia.edu>
2016-05-02 01:48:34 -0400

    Best time to buy and sell stock- easy

--- Easy/BestStock.java
+++ Easy/BestStock.java
@@ -1,8 +1,12 @@
 public class BestStock {
+    //Input: An array for which the ith element is the price of a given stock on day i.
+
+    //You are permitted to complete at most one transaction, design an algorithm to find the maximum profit.
+    //(i.e. buy one and sell one share of the stock)
     public int maxProfit(int[] prices) 
     {
-        if(prices.length<=1) return 0;
-        int change[] = new int[prices.length]; //change in stock prices (don't even need a new array)
+        if(prices.length<=1) return 0; //Base case
+        int change[] = new int[prices.length]; //to store change in stock prices (don't even need a new array)
         int maxSoFar = 0, maxEndingHere = 0;
         for(int i = 1; i< prices.length ; i++)
         {
