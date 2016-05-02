02fb4fb28e243ceed7918b79406ce2730f558820
Sania Arif <sania.arif@columbia.edu>
2016-05-02 01:51:06 -0400

    Balanced binary tree?

---
+++ Easy/BalancedBin.java
@@ -0,0 +1,42 @@
+public class BalancedBin {
+
+    //METHOD 1:
+    /* public boolean isBalanced(TreeNode root)
+    {
+        if(root==null) return true;
+        if(Math.abs(checkHeight(root.left) - checkHeight(root.right)) > 1)
+            return false;
+        else return(isBalanced(root.left) && isBalanced(root.right));
+    }
+    
+    public int checkHeight(TreeNode root)
+    {
+        if(root==null) return 0;
+        return 1+ (Math.max(checkHeight(root.left), checkHeight(root.right)));
+    } */
+    
+    //METHOD 2: More efficient:
+    public boolean isBalanced(TreeNode root)
+    {
+        if(checkHeight(root) == -1) return false;
+        return true;
+    }
+    
+    public int checkHeight(TreeNode root)
+    {
+        if(root==null) return 0;
+        
+        int left = checkHeight(root.left);
+        if(left == -1) return -1;
+        
+        int right = checkHeight(root.right);
+        if(right == -1) return -1;
+        
+        int diff = Math.abs(left-right);
+        if(diff>1) return -1;
+        
+        else return (1+ Math.max(left, right));
+        
+    }
+    
+}
