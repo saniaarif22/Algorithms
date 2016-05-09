c4c5d5179e378de3f10bdb9dcb90b10dfb32a9a5
Sania Arif <sania.arif@columbia.edu>
2016-05-09 00:24:05 -0400

    Path sum = number

--- Easy/PathSum.java
+++ Easy/PathSum.java
@@ -1,9 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
-public class PathSum 
+//Given a binary tree and a number, determine if the tree has a root-to-leaf path 
+//such that adding the values along the path equals to the given number.
+
+public class Solution 
 {
     public boolean hasPathSum(TreeNode root, int sum) 
     {
-        if(root == null) return false; 
+        if(root == null) return false; //Not true if sum=0 (that's how the method is)
 
         if(root.left == null && root.right == null && sum - root.val == 0) return true;
 
