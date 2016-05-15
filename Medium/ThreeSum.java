33fe090e6e31ecc39ef52cb800bdd59d242b7008
Sania Arif <sania.arif@columbia.edu>
2016-05-14 23:16:56 -0400

    Three sum

--- Medium/ThreeSum.java
+++ Medium/ThreeSum.java
@@ -1,4 +1,6 @@
-public class Solution {
+public class ThreeSum {
+    //Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+    //Find all unique triplets in the array which gives the sum of zero.
     public List<List<Integer>> threeSum(int[] num) 
     {
         Arrays.sort(num);
